bb2308845a8641e64fdea2f8be4c6c166a05d2dd
Yehonatan Deri <yonatander@gmail.com>
2026-03-13 18:53:25 +0200

    End of section 5

---
+++ client/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,27 @@
+import { Button, Card, CardContent, CardMedia, Typography } from "@mui/material"
+
+type Prop = {
+    activity: Activity
+    cancelSelectActivity: () => void;
+    openForm: (id: string) => void
+}
+
+export default function ActivityDetails({ activity, cancelSelectActivity, openForm }: Prop) {
+    return (
+        <Card sx={{ borderRadius: 3 }}>
+            <CardMedia
+                component='img'
+                src={`/images/categoryImages/${activity.category}.jpg`}
+            />
+            <CardContent>
+                <Typography variant="h5">{activity.title}</Typography>
+                <Typography variant="subtitle1">{activity.date}</Typography>
+                <Typography variant="body1">{activity.description}</Typography>
+            </CardContent>
+            <CardContent>
+                <Button onClick={() => openForm(activity.id)} color="primary">Edit</Button>
+                <Button onClick={cancelSelectActivity} color="inherit">Cancel</Button>
+            </CardContent>
+        </Card>
+    )
+}
